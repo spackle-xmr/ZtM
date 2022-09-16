fb36986cbaed7d4aa039bf1c3dc87f27cb076936
spackle-xmr <93713512+spackle-xmr@users.noreply.github.com>
2022-09-15 23:18:23 -0700

    Update Chapter3.py
    Add CLSAG

--- Chapter3.py
+++ Chapter3.py
@@ -257,3 +257,116 @@ if c[0] == cprime[count]:
     print('Valid')
 
 '''
+
+# 3.6 CLSAG
+# 2d set of public keys K_i_j, with knowledge of m private key k_pi_j for index i = pi
+m="Someone among us said this."
+count = 10 # key count (size of each dimension of R, for a total of count^2 keys)
+pi= count-1 #Set pi as final index to simplify example code. Same effect as a random selection with the elements rearranged.
+k_pi_j=[dumb25519.random_scalar() for i in range(count)]
+K_pi_j= [k_pi_j[i] * G for i in range(count)]
+R= [[dumb25519.random_point() for i in range(count)] for j in range(count)]
+R[pi]=K_pi_j
+
+# Signature
+
+# 1. Calculate key images K_tilde_j
+K_tilde_j= [k_pi_j[j] * dumb25519.hash_to_point(K_pi_j[0]) for j in range(count)]
+
+# 2. Generate random numbers alpha and r_i_j except for the pi set of j 
+alpha=dumb25519.random_scalar()
+r_i=[dumb25519.random_scalar() for j in range(count)]
+r_i[pi]=0
+c=[0]*count
+
+# 3. Calculate aggreate public keys W_i for all i, and aggreate key image W_tilde
+W_i=[0]*count
+j_terms=[0]*count
+T_j_raw=['CLSAG_']*count
+T_j=[f'{elm}{index+1}' for index, elm in enumerate(T_j_raw)]
+#T_j=['CLSAG_1', 'CLSAG_2', 'CLSAG_3', 'CLSAG_4', 'CLSAG_5']
+T_c=('CLSAG_c')
+
+# Aggreate public keys W_i for all i
+for i in range(count):
+    #get sum of j terms
+    for j in range(count):
+        j_terms[j]=dumb25519.hash_to_scalar(T_j[j], R, K_tilde_j) * R[i][j]
+        if j == 0:
+            j_sum = j_terms[j]
+        j_sum = j_sum + j_terms[j]
+    W_i[i]=j_sum
+
+
+# Aggreate key image W_tilde
+for j in range(count):
+        j_terms[j]=dumb25519.hash_to_scalar(T_j[j], R, K_tilde_j) * K_tilde_j[j]
+        if j == 0:
+            j_sum = j_terms[j]
+        j_sum = j_sum + j_terms[j]
+W_tilde=j_sum
+
+# Aggregate private key w_pi
+for j in range(count):
+        j_terms[j]=dumb25519.hash_to_scalar(T_j[j], R, K_tilde_j) * k_pi_j[j]
+        if j == 0:
+            j_sum = j_terms[j]
+        j_sum = j_sum + j_terms[j]
+w_pi=j_sum
+
+# 4. Compute challenge for pi+1 (note that pi+1 is index 0)
+c[0]=dumb25519.hash_to_scalar(T_c, R, m, alpha * G, alpha * dumb25519.hash_to_point(K_pi_j[0]))
+
+# 5. Compute challenge set
+for i in range(pi):
+    c[i+1]=dumb25519.hash_to_scalar(T_c, R, m, r_i[i] * G + c[i] * W_i[i], r_i[i] * dumb25519.hash_to_point(R[i][0]) + c[i] * W_tilde)
+
+# 6. Define r_pi
+r_i[pi]=alpha - c[pi] * w_pi
+
+
+# Verification
+cprime=[0]*(count+1)
+
+# 1. zero check all l * K_tilde_j
+for index in range(count):
+    zero_check= l * K_tilde_j[index]
+    if zero_check.x != 0:
+        print('Zero Check Fail')
+        break
+    if index == count-1:
+        print('Zero Check Passed')
+
+# 2. Calculate aggreate public keys W_i for all i, and aggreate key image W_tilde (same as Signature step 3, without knowledge of private keys)
+W_i=[0]*count
+j_terms=[0]*count
+
+# Aggreate public keys W_i for all i
+for i in range(count):
+    #get sum of j terms
+    for j in range(count):
+        j_terms[j]=dumb25519.hash_to_scalar(T_j[j], R, K_tilde_j) * R[i][j]
+        if j == 0:
+            j_sum = j_terms[j]
+        j_sum = j_sum + j_terms[j]
+    W_i[i]=j_sum
+
+# Aggreate key image W_tilde
+for j in range(count):
+        j_terms[j]=dumb25519.hash_to_scalar(T_j[j], R, K_tilde_j) * K_tilde_j[j]
+        if j == 0:
+            j_sum = j_terms[j]
+        j_sum = j_sum + j_terms[j]
+W_tilde=j_sum
+
+# 3. Compute cprime set
+cprime[0] = c[0] #start with signature value
+# Exact same calculation performed in Signature step 5, but expanded to calculate signature challenge value twice
+for i in range(count):
+    cprime[i+1]=dumb25519.hash_to_scalar(T_c, R, m, r_i[i] * G + cprime[i] * W_i[i], r_i[i] * dumb25519.hash_to_point(R[i][0]) + cprime[i] * W_tilde)
+
+# 4. Check c_1 == cprime_1
+if c[0] == cprime[count]:
+    print('Valid')
+
+
